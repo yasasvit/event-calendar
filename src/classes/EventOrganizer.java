d4ff8d52cc8d10de43c1929652c6d03a673fbc5d
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-09-30 21:31:27 -0400

    EventOrganizer.java

--- src/classes/EventOrganizer.java
+++ src/classes/EventOrganizer.java
@@ -1,4 +1,36 @@
 package classes;
-
+import java.util.Scanner;
 public class EventOrganizer {
+    private boolean isRunning;
+
+    public EventOrganizer() {
+        System.out.println("Event Organizer running....");
+        this.isRunning = true;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (isRunning) {
+            System.out.print("Enter a command: ");
+            String input = scanner.nextLine();
+
+            processCommand(input);
+        }
+
+        System.out.println("Event Organizer terminated");
+        scanner.close();
+    }
+
+    private void processCommand(String command) {
+        if (command.equalsIgnoreCase("Q")) {
+            isRunning = false;
+        } else if (command.equalsIgnoreCase("Add Event:")) {
+            // Handle adding an event
+        } else if (command.equalsIgnoreCase("Remove Event")) {
+            // Handle removing an event
+        } else {
+            System.out.println("Unknown command: " + command);
+        }
+    }
 }
