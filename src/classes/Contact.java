02848144fe43115661fd80d0944aa155691550d4
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-10-01 11:11:58 -0400

    Update Contact.java

--- src/classes/Contact.java
+++ src/classes/Contact.java
@@ -6,8 +6,9 @@ public class Contact {
 
     // Constructor
     // Constants for valid department names and email regex pattern
-    private static final String[] VALID_DEPARTMENTS = {"Computer Science", "Mathematics", "Physics"};
-    private static final String EMAIL_REGEX_PATTERN = "^[A-Za-z0-9+_.-]+@(.+)$";
+    private static final String[] validDepartments = {"Computer Science", "Mathematics" +
+            "Information Technology and Informatics", "Electrical Engineering", "Business Analytics and Information Technology"};
+    private static final String emailPattern = "^[A-Za-z0-9+_.-]+@(.+)$";
 
     // Constructor
     public Contact(Department department, String email) {
@@ -19,7 +20,7 @@ public class Contact {
 
     // Method to check if the department name is valid
     private boolean isDepartmentValid() {
-        for (String validDepartment : VALID_DEPARTMENTS) {
+        for (String validDepartment : validDepartments) {
             if (validDepartment.equals(department.getName())) {
                 return true;
             }
@@ -29,7 +30,7 @@ public class Contact {
 
     // Method to check if the email is valid
     private boolean isEmailValid() {
-        return email.matches(EMAIL_REGEX_PATTERN);
+        return email.matches(emailPattern);
     }
 
     // isValid method to check if both department and email are valid
