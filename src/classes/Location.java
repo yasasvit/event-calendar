0d1d1e6aa530c9cb614545ac5828b7c0ef2cd695
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-10-02 00:37:09 -0400

    Update Location.java

--- src/classes/Location.java
+++ src/classes/Location.java
@@ -5,7 +5,9 @@ This class contains the information for the 6 locations
 @author Pranav Gummaluri, Yasasvi Tallapaneni
 */
 
+
 public enum Location {
+    //location data
     HLL114("HLL114", "Hill Center, Busch"),
     ARC103("ARC103", "Allison Road Classroom, Busch"),
     BE_AUD("BE_AUD", "Beck Hall, Livingston"),
@@ -13,14 +15,17 @@ public enum Location {
     AB2225("AB2225", "Academic Building, College Avenue"),
     MU302("MU302", "Murray Hall, College Avenue");
 
+    //instance variables
     private final String roomNumber;
     private final String buildingAndCampus;
 
+    //constructor
     private Location(String roomNumber, String buildingAndCampus) {
         this.roomNumber = roomNumber;
         this.buildingAndCampus = buildingAndCampus;
     }
 
+    //Getters
     public String getRoomNumber() {
         return roomNumber;
     }
@@ -29,6 +34,7 @@ public enum Location {
         return buildingAndCampus;
     }
 
+    //boolean for checking if the information matches
     public boolean equals(Location otherLocation) {
         return this == otherLocation;
     }
